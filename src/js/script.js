f4a3a68715c3bd7bae2a6927c72a676316a0be2d
ivancraze <ivancraze@yandex.ru>
2021-02-05 01:51:51 +0400

    add promo section

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log("");
+$(function(){
+
+  $(".promo-slider").slick({
+    dots: true,
+    prevArrow: '<button class="promo-slider__btn promo-slider__btn-prev"><img src="img/icons/arrow_prev.svg"></button>',
+    nextArrow: '<button class="promo-slider__btn promo-slider__btn-next"><img src="img/icons/arrow_next.svg"></button>'
+  });
+
+});
