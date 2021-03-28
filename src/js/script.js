3e88ecd78db534b4a692c231330004e74f016f1e
ivancraze <ivancraze@yandex.ru>
2021-03-28 15:06:59 +0400

    remake tabs from jquery to vanilla js

--- src/js/script.js
+++ src/js/script.js
@@ -5,15 +5,27 @@ $(function(){
     prevArrow: '<button class="promo-slider__btn promo-slider__btn-prev"><img src="img/icons/arrow_prev.svg"></button>',
     nextArrow: '<button class="promo-slider__btn promo-slider__btn-next"><img src="img/icons/arrow_next.svg"></button>'
   });
-
-  $('.search-tabs__link').on('click', function(e) {
-    e.preventDefault();
-    
-    $('.search-tabs__link').removeClass('search-tabs__link-active');
-    $('.search-content__item').removeClass('search-content__item-active');
-
-    $(this).addClass('search-tabs__link-active');
-    $($(this).attr('href')).addClass('search-content__item-active');
-  });
 });
 
+
+// Tabs
+const tabsLinks = document.querySelectorAll('.search-tabs__link');
+const contentItems = document.querySelectorAll('.search-content__item');
+
+for (let i = 0; i < tabsLinks.length; i++) {
+  tabsLinks[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    let activeTabAttr = e.target.getAttribute("href");
+    for (let j = 0; j < tabsLinks.length; j++) {
+      let contentAttr = contentItems[j].getAttribute("id");
+
+      if (activeTabAttr === contentAttr) {
+        tabsLinks[j].classList.add("search-tabs__link-active");
+        contentItems[j].classList.add("search-content__item-active"); 
+      } else {
+        tabsLinks[j].classList.remove("search-tabs__link-active");
+        contentItems[j].classList.remove("search-content__item-active");
+      }
+    }
+  });
+}
