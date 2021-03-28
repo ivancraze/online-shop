c605fe5d9aca73b9f9fce04c381fd1508b00bab3
ivancraze <ivancraze@yandex.ru>
2021-03-28 14:46:47 +0400

    add search tabs section

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,14 @@ $(function(){
     nextArrow: '<button class="promo-slider__btn promo-slider__btn-next"><img src="img/icons/arrow_next.svg"></button>'
   });
 
+  $('.search-tabs__link').on('click', function(e) {
+    e.preventDefault();
+    
+    $('.search-tabs__link').removeClass('search-tabs__link-active');
+    $('.search-content__item').removeClass('search-content__item-active');
+
+    $(this).addClass('search-tabs__link-active');
+    $($(this).attr('href')).addClass('search-content__item-active');
+  });
 });
+
