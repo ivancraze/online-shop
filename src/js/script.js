b2f65692d5f8fba41f4ed9c208b8349ddee4b17e
ivancraze <ivancraze@yandex.ru>
2021-03-30 14:54:19 +0400

    add popular products section

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,21 @@
 $(function(){
-
+//Sliders
   $(".promo-slider").slick({
     dots: true,
     prevArrow: '<button class="promo-slider__btn promo-slider__btn-prev"><img src="img/icons/arrow_prev.svg"></button>',
     nextArrow: '<button class="promo-slider__btn promo-slider__btn-next"><img src="img/icons/arrow_next.svg"></button>'
   });
+
+  $(".product-slider").slick({
+    slidesToShow: 4,
+    prevArrow: '<button class="product-slider__btn product-slider__btn-prev"><img src="img/icons/arrow_black_prev.svg"></button>',
+    nextArrow: '<button class="product-slider__btn product-slider__btn-next"><img src="img/icons/arrow_black_next.svg"></button>'
+  });
 });
 
 
 // Tabs
+// Search tab
 const tabsLinks = document.querySelectorAll('.search-tabs__link');
 const contentItems = document.querySelectorAll('.search-content__item');
 
@@ -27,5 +34,40 @@ for (let i = 0; i < tabsLinks.length; i++) {
         contentItems[j].classList.remove("search-content__item-active");
       }
     }
+  });
+}
+
+const productTabsLinks = document.querySelectorAll('.tab');
+const productContentItems = document.querySelectorAll('.tabs-content');
+
+for (let i = 0; i < productTabsLinks.length; i++) {
+  productTabsLinks[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    let activeTabAttr = e.target.getAttribute("href");
+
+    for (let j = 0; j < productTabsLinks.length; j++) {
+      let contentAttr = productContentItems[j].getAttribute("id");
+
+      if (activeTabAttr === contentAttr) {
+        productTabsLinks[j].classList.add("tab--active");
+        productContentItems[j].classList.add("tabs-content--active"); 
+      } else {
+        productTabsLinks[j].classList.remove("tab--active");
+        productContentItems[j].classList.remove("tabs-content--active");
+      }
+    }
+  });
+}
+
+
+
+
+// Popular
+// favourite button
+const favouriteBtn = document.querySelectorAll('.product-item__favourite');
+for (let i = 0; i < favouriteBtn.length; i++) {
+  favouriteBtn[i].addEventListener("click", (e) => {
+    e.preventDefault();
+    favouriteBtn[i].classList.toggle('product-item__favourite--active');
   });
 }
