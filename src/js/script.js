531f5d9b5d0b5440e6601aa34d4e7e804619d9ab
ivancraze <ivancraze@yandex.ru>
2021-03-30 18:42:40 +0400

    tabs script refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-$(function(){
 //Sliders
+$(function(){
   $(".promo-slider").slick({
     dots: true,
     prevArrow: '<button class="promo-slider__btn promo-slider__btn-prev"><img src="img/icons/arrow_prev.svg"></button>',
@@ -16,50 +16,48 @@ $(function(){
 
 // Tabs
 // Search tab
-const tabsLinks = document.querySelectorAll('.search-tabs__link');
-const contentItems = document.querySelectorAll('.search-content__item');
+// const tabsLinks = document.querySelectorAll('.search-tabs__link');
+// const contentItems = document.querySelectorAll('.search-content__item');
 
-for (let i = 0; i < tabsLinks.length; i++) {
-  tabsLinks[i].addEventListener("click", (e) => {
-    e.preventDefault();
-    let activeTabAttr = e.target.getAttribute("href");
-    for (let j = 0; j < tabsLinks.length; j++) {
-      let contentAttr = contentItems[j].getAttribute("id");
+// for (let i = 0; i < tabsLinks.length; i++) {
+//   tabsLinks[i].addEventListener("click", (e) => {
+//     e.preventDefault();
+//     let activeTabAttr = e.target.getAttribute("href");
+//     for (let j = 0; j < tabsLinks.length; j++) {
+//       let contentAttr = contentItems[j].getAttribute("id");
 
-      if (activeTabAttr === contentAttr) {
-        tabsLinks[j].classList.add("search-tabs__link-active");
-        contentItems[j].classList.add("search-content__item-active"); 
-      } else {
-        tabsLinks[j].classList.remove("search-tabs__link-active");
-        contentItems[j].classList.remove("search-content__item-active");
-      }
-    }
-  });
-}
+//       if (activeTabAttr === contentAttr) {
+//         tabsLinks[j].classList.add("search-tabs__link-active");
+//         contentItems[j].classList.add("search-content__item-active"); 
+//       } else {
+//         tabsLinks[j].classList.remove("search-tabs__link-active");
+//         contentItems[j].classList.remove("search-content__item-active");
+//       }
+//     }
+//   });
+// }
 
-const productTabsLinks = document.querySelectorAll('.tab');
-const productContentItems = document.querySelectorAll('.tabs-content');
+// const productTabsLinks = document.querySelectorAll('.tab');
+// const productContentItems = document.querySelectorAll('.tabs-content');
 
-for (let i = 0; i < productTabsLinks.length; i++) {
-  productTabsLinks[i].addEventListener("click", (e) => {
-    e.preventDefault();
-    let activeTabAttr = e.target.getAttribute("href");
+// for (let i = 0; i < productTabsLinks.length; i++) {
+//   productTabsLinks[i].addEventListener("click", (e) => {
+//     e.preventDefault();
+//     let activeTabAttr = e.target.getAttribute("href");
 
-    for (let j = 0; j < productTabsLinks.length; j++) {
-      let contentAttr = productContentItems[j].getAttribute("id");
+//     for (let j = 0; j < productTabsLinks.length; j++) {
+//       let contentAttr = productContentItems[j].getAttribute("id");
 
-      if (activeTabAttr === contentAttr) {
-        productTabsLinks[j].classList.add("tab--active");
-        productContentItems[j].classList.add("tabs-content--active"); 
-      } else {
-        productTabsLinks[j].classList.remove("tab--active");
-        productContentItems[j].classList.remove("tabs-content--active");
-      }
-    }
-  });
-}
-
-
+//       if (activeTabAttr === contentAttr) {
+//         productTabsLinks[j].classList.add("tab--active");
+//         productContentItems[j].classList.add("tabs-content--active"); 
+//       } else {
+//         productTabsLinks[j].classList.remove("tab--active");
+//         productContentItems[j].classList.remove("tabs-content--active");
+//       }
+//     }
+//   });
+// }
 
 
 // Popular
@@ -69,5 +67,48 @@ for (let i = 0; i < favouriteBtn.length; i++) {
   favouriteBtn[i].addEventListener("click", (e) => {
     e.preventDefault();
     favouriteBtn[i].classList.toggle('product-item__favourite--active');
+  });
+}
+
+//Activate tabs in search section
+tabs('.search-tabs__title', '.search-tabs__content-item', '.search-tabs__header', 'active');
+//Activate tabs in products section
+// tabs('.search-tabs__title', '.search-tabs__content-item', '.search-tabs__header', 'active');
+
+//Tabs function
+function tabs (tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+
+  const tabs = document.querySelectorAll(tabsSelector);
+  const tabsContent = document.querySelectorAll(tabsContentSelector);
+  const tabsParent = document.querySelector(tabsParentSelector);
+  
+  function hideTabContent() {
+      tabsContent.forEach(item => {
+        item.classList.remove(activeClass);
+      });
+
+      tabs.forEach(item => {
+        item.classList.remove(activeClass);
+      });
+  }
+  
+  function showTabContent(i = 0) {
+      tabsContent[i].classList.add(activeClass);
+      tabs[i].classList.add(activeClass);
+  }
+  
+  hideTabContent();
+  showTabContent();
+  
+  tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target && target.classList.contains(tabsSelector.slice(1))) {
+    tabs.forEach((item, i) => {
+      if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+          }
+      });
+    }
   });
 }
