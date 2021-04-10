7436870e12ba0c9d859181d0fcec685e69f8d93d
ivancraze <ivancraze@yandex.ru>
2021-04-10 16:57:40 +0400

    add catalog filters and minor css fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
-//Sliders
 $(function(){
+  //Sliders
   $(".promo-slider").slick({
     dots: true,
     prevArrow: '<button class="promo-slider__btn promo-slider__btn-prev"><img src="img/icons/arrow_prev.svg"></button>',
@@ -11,7 +11,10 @@ $(function(){
     prevArrow: '<button class="product-slider__btn product-slider__btn-prev"><img src="img/icons/arrow_black_prev.svg"></button>',
     nextArrow: '<button class="product-slider__btn product-slider__btn-next"><img src="img/icons/arrow_black_next.svg"></button>'
   });
+  //FormStyler
+  $('.filter-style').styler();
 });
+
 //Tabs 
 //Activate tabs in search section
 tabs('.search-tabs__title', '.search-tabs__content-item', '.search-tabs__header', 'active');
@@ -19,13 +22,17 @@ tabs('.search-tabs__title', '.search-tabs__content-item', '.search-tabs__header'
 tabs('.popular-tabs__title', '.popular-tabs__content-item', '.popular-tabs__header', 'active');
 //Activate tabs in suggest section
 tabs('.suggest-tabs__title', '.suggest-tabs__content-item', '.suggest-tabs__header', 'active');
+// Activate tabs in aside filters from catalog page
+tabs('.aside-filter-tabs__title', '.aside-filter-tabs__content-item', '.aside-filter-tabs__header', 'active');
 
 function tabs (tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
 
   const tabs = document.querySelectorAll(tabsSelector);
   const tabsContent = document.querySelectorAll(tabsContentSelector);
   const tabsParent = document.querySelector(tabsParentSelector);
-  
+
+  if (tabs.length === 0 || tabsContent.length === 0 || tabsParent.length === 0) return;
+
   function hideTabContent() {
       tabsContent.forEach(item => {
         item.classList.remove(activeClass);
@@ -46,23 +53,28 @@ function tabs (tabsSelector, tabsContentSelector, tabsParentSelector, activeClas
   
   tabsParent.addEventListener('click', (event) => {
     const target = event.target;
-    if (target && target.classList.contains(tabsSelector.slice(1))) {
     tabs.forEach((item, i) => {
       if (target == item) {
           hideTabContent();
           showTabContent(i);
           }
       });
-    }
   });
 }
+
 // Popular
 // favourite button
 const favouriteBtn = document.querySelectorAll('.product-item__favourite');
-
 for (let i = 0; i < favouriteBtn.length; i++) {
   favouriteBtn[i].addEventListener("click", (e) => {
     e.preventDefault();
     favouriteBtn[i].classList.toggle('product-item__favourite--active');
   });
 }
+
+//Content
+//Filters accordeon
+$('.filter__item-drop').on('click', function(){
+  $(this).toggleClass('filter__item-drop--active');
+  $(this).next().slideToggle(200);
+})
